4203b7de3e1ba1d8f84d773dff93bc59510ff8b5
Sreesh Nair <sreeshbk@gmail.com>
2025-07-09 13:23:05 +0530

    azure function added

--- azure_functions/az/db_config.py
+++
@@ -1,8 +0,0 @@
-# Database connection settings for PostgreSQL
-import os
-
-DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_PORT = int(os.environ.get("DB_PORT", 5432))
